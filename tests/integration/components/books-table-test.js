25caffd0ca15c9d385685db0b62996c0ec329a3d
suba2002harini <subaharini2002@gmail.com>
2022-11-08 18:27:43 +0530

    changes in test file

--- tests/integration/components/books-table-test.js
+++ tests/integration/components/books-table-test.js
@@ -5,22 +5,4 @@ import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | books-table', function (hooks) {
   setupRenderingTest(hooks);
-
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`<BooksTable />`);
-
-    assert.dom(this.element).hasText('');
-
-    // Template block usage:
-    await render(hbs`
-      <BooksTable>
-        template block text
-      </BooksTable>
-    `);
-
-    assert.dom(this.element).hasText('template block text');
-  });
 });
--- tests/integration/components/nav-test.js
+++ tests/integration/components/nav-test.js
@@ -5,22 +5,4 @@ import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | nav', function (hooks) {
   setupRenderingTest(hooks);
-
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`<Nav />`);
-
-    assert.dom(this.element).hasText('');
-
-    // Template block usage:
-    await render(hbs`
-      <Nav>
-        template block text
-      </Nav>
-    `);
-
-    assert.dom(this.element).hasText('template block text');
-  });
 });
