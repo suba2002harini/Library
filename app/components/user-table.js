b3bc1b00a2daacec628e2abb5766d58f1b99f65f
suba2002harini <subaharini2002@gmail.com>
2022-11-10 13:02:17 +0530

    edit dailog box added

--- app/components/user-table.js
+++ app/components/user-table.js
@@ -3,40 +3,39 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 export default class UserTableComponent extends Component {
-    @tracked ref;
-    @tracked uname;
-    @tracked uemail;
-    @tracked umobile;
-    @tracked uid;
-    @service store;
-    @action info(event)
-    {       
-         this.ref=event.target.id;
-         console.log(this.args.user);
-         const temp=this.store.peekRecord('user', this.ref);
-         this.uname=temp.name;
-         this.uemail=temp.email;
-         this.umobile=temp.mobile;
-         this.uid=temp.uid;
-         console.log(this.uname);
-         
-    }
-    adding(email, name, num) {
-             console.log("edit function");
-             this.store.findRecord('user', this.ref).then(function(post) {
-              post.name = name;
-              post.email = email;
-              post.mobile = num;
-              post.save(); 
-    });
-    }
-    @action update(event)
-    {    
-        event.preventDefault();
-          this.adding(this.uemail, this.uname, this.umobile);
-          this.uemail = '';
-          this.uname = '';
-          this.umobile = '';
-          this.uid= '';
-    }
+@tracked ref;
+@tracked uname;
+@tracked uemail;
+@tracked umobile;
+@tracked uid;
+@service store;
+@action info(event)
+{       
+    this.ref=event.target.id;
+    console.log(this.args.user);
+    const temp=this.store.peekRecord('user', this.ref);
+    this.uname = temp.name;
+    this.uemail = temp.email;
+    this.umobile = temp.mobile;
+    this.uid = temp.uid;
+    console.log(this.uname);
+        
+}
+adding(email, name, num) {
+    this.store.findRecord('user', this.ref).then(function(post ) {
+    post.name = name;
+    post.email = email;
+    post.mobile = num;
+    post.save(); 
+});
+}
+@action update(event)
+{    
+    event.preventDefault();
+        this.adding(this.uemail, this.uname, this.umobile);
+        this.uemail = '';
+        this.uname = '';
+        this.umobile = '';
+        this.uid= '';
+}
 }
