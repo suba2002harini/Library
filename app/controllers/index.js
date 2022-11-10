3d8496e8c36d8f05b8bdf786a192339231bfe624
suba2002harini <subaharini2002@gmail.com>
2022-11-10 12:56:02 +0530

    edit dailog box added

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,32 +5,18 @@ export default class IndexController extends Controller {
   @service store;
   @action
   addMessage(id, email, name, num) {
-    const newPost = this.store.createRecord('user', {
-      uid: id,
-      name: name,
-      email: email,
-      mobile: num,
+      this.store.push({
+      data: [{
+        id: id,          
+        type: 'user',
+        attributes: {
+          uid: id,
+          name: name,
+          email: email,
+          mobile: num,
+        },
+      }]
     });
-
-    try {
-      newPost.save();
-      console.log(newPost);
-      //      this.router.transitionTo('rental', newPost.id);
-      return newPost;
-    } catch (error) {
-      console.log(error);
-    }
     return this.store.findAll('user');
-    //   this.messages = [
-    //     ...this.messages,
-    //     {
-    //       name: name,
-    //       email: email,
-    //       content:num,
-    //     },
-    //   ];
   }
-
-  // @tracked messages = [
-  // ];
 }
